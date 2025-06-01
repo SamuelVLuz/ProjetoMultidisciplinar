89b8d03519fbfabf70f5242cf54a0af6eb9be5ca
Samuel Vinícius da Luz <63885675+SamuelVLuz@users.noreply.github.com>
2025-05-31 23:05:22 -0300

    Entropy
    Mostra a entropia da senha enquanto está digitando

--- keycloak-playground/keycloak/themes/registrocivil/theme/registrocivil/login/resources/js/entropy.js
+++ keycloak-playground/keycloak/themes/registrocivil/theme/registrocivil/login/resources/js/entropy.js
@@ -1,10 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
   const passwordField = document.getElementById('password');
-  const passwordPreview = document.getElementById('passwordEntropy');
-
-  if (passwordField && passwordPreview) {
+  const passwordEntropy = document.getElementById('passwordEntropy');
+  
+  function computeEntropy(password) {
+    if (password.length === 0) return 0; // Handle empty input
+    const uniqueChars = new Set(password).size;
+    const entropyPerChar = Math.log2(uniqueChars);
+    const entropy = password.length * entropyPerChar;
+   return entropy;
+  }
+  
+  if (passwordField && passwordEntropy) {
     passwordField.addEventListener('input', function () {
-      passwordPreview.textContent = passwordField.value;
+      const entropy = computeEntropy(passwordField.value);
+      passwordEntropy.textContent = entropy.toFixed(2); // Show 2 decimal places
     });
   }
 });
